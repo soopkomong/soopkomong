9b6a63a703fa2b9d1d8c2f99f7d40acc2101ce66
지은성 <ddrr2852@gmail.com>
2026-02-27 10:08:02 +0900

    feat-home-mapbox

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:soopkomong/firebase_options.dart';
+import 'core/app_initializer.dart';
 import 'core/router/app_router.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // 앱 관련 모든 비동기 설정 초기화
+  await AppInitializer.init();
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
